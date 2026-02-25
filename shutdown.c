f3944b84898a6af30d3b72b5880fdfcbc52f7030
hyang-gif <hyang@kean.edu>
2026-02-25 18:37:43 +0000

    [leave a message to describe this submission]

---
+++ shutdown.c
@@ -0,0 +1,13 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+/* Remove the following code when heading to next task */
+/********** start **********/
+
+/********** end **********/
+int main (int arc, char *argv[])
+{
+printf(1, "\nBYEBYE XV6\n\n");
+shutdown();
+exit();
+}
